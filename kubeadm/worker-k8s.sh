d7efed2e4bb8a089ac8318e5994fe225abc98ce2
telechi <118616238+telechi@users.noreply.github.com>
2023-02-04 17:05:52 -0500

    Update worker-k8s.sh

--- kubeadm/worker-k8s.sh
+++ kubeadm/worker-k8s.sh
@@ -101,4 +101,4 @@ systemctl enable kubelet.service
 
 kubeadm join 10.0.0.11:6443 --token 03em5o.agjpy9wbj98izbpn \
         --discovery-token-ca-cert-hash sha256:92a1cf1e452961a550f05eff32f25500fc9bcfd5d5f771689e8ee21b6a0da243
-
+su - ubuntu
